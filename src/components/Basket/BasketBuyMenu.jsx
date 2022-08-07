f4b41a1cd6233d90efd1f51ff30e37985929116f
Bula2 <bulaev123da@gmail.com>
2022-08-07 04:31:09 +0200

    update basket component

--- src/components/Basket/BasketBuyMenu.jsx
+++ src/components/Basket/BasketBuyMenu.jsx
@@ -4,14 +4,22 @@ const BasketBuyMenu = ({basketItems}) => {
     debugger
     return (
         <div className={cls.buy_menu}>
-            <div className={cls.buy_menu_info}>
-                <ul>
-                    {basketItems.map(item => {
-                        return(
-                            <li>{item.title}</li>
-                        )
-                    })}
-                </ul>
+            <div className={cls.header_buy_menu}>
+                <div>
+                    <span>Список игр</span>
+                </div>
+                <div className={cls.buy_menu_info}>
+                    <ul>
+                        {basketItems.map(item => {
+                            return (
+                                <li>{item.title}</li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+            <div className={cls.buy_menu_button}>
+                <button>Купить</button>
             </div>
             <div>
 
